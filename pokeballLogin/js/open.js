5da33719726094fb9128d1425514787f8821b13d
Carlos Teruel <cartegar@gmail.com>
2016-09-21 07:48:26 +0200

    Update open.js

--- pokeballLogin/js/open.js
+++ pokeballLogin/js/open.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-	$("#centro").click(function(){
+	$("#center").click(function(){
 	
 		$("#content").css({"height": "650px"});
-		$("#centro").css({"display": "none"});
+		$("#center").css({"display": "none"});
 		$("#login-content").css({"height": "190px"});
 		$("input[type=" + "submit" + "]").css({"top": "0"});
 		$("#content").css({"z-index": "0"});
